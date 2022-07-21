9f09f768d7b469124a13504c71b6de8d20d7878d
unknown <4friizal@gmail.com>
2022-07-21 23:29:56 +0700

    CRUD APi

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,28 +1,51 @@
 import React from "react";
-import { useState } from "react";
 
-const handleChange = (e) => {
-  console.log(e.target.value);
-};
-
-export default function Form() {
+const Form = (props) => {
   return (
-    <form className="flex justify-between">
-      <div>
-        <input
-          type="text"
-          className="bg-sky-50 border border-sky-300 text-sky-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[22rem] p-2.5 dark:bg-sky-700 dark:border-sky-600 dark:placeholder-sky-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Add to list"
-          required=""
-          onChange={handleChange}
-        />
-      </div>
-      <button
-        type="submit"
-        className="text-white bg-blue-700 py-2 px-10 rounded-lg"
-      >
-        Add
-      </button>
+    <form className="my-5" onSubmit={props.onSubmit}>
+      {props.isUpdate ? (
+        <div className="flex justify-around">
+          <input
+            type="text"
+            className="bg-sky-50 border border-sky-300 text-sky-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-sky-700 dark:border-sky-600 dark:placeholder-sky-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Add to list"
+            required=""
+            value={props.content}
+            onChange={props.onChange}
+          />
+          <button
+            type="submit"
+            className="text-white bg-blue-700 py-2 px-2 rounded-lg"
+          >
+            Update
+          </button>
+          <button
+            className="text-white bg-red-700 py-2 px-2 rounded-lg"
+            onClick={props.cancelUpdate}
+          >
+            Cancel
+          </button>
+        </div>
+      ) : (
+        <div className="flex">
+          <input
+            type="text"
+            className="bg-sky-50 border border-sky-300 text-sky-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[22rem] p-2.5 dark:bg-sky-700 dark:border-sky-600 dark:placeholder-sky-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Add to list"
+            required=""
+            value={props.content}
+            onChange={props.onChange}
+          />
+          <button
+            type="submit"
+            className="text-white bg-blue-700 py-2 px-10 ml-1 rounded-lg"
+          >
+            Add
+          </button>
+        </div>
+      )}
     </form>
   );
-}
+};
+
+export default Form;
--- src/components/ListTodo.jsx
+++ src/components/ListTodo.jsx
@@ -1,49 +1,160 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { AiFillEdit, AiOutlineDelete } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 import Form from "./Form";
 
 const ListTodo = () => {
+  const navigate = useNavigate();
+  const [content, setContent] = useState("");
+  const [idTodo, setIdTodo] = useState(null);
+  const [isUpdate, setIsUpdate] = useState(false);
   const [task, setTask] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
     fetchData();
   }, []);
 
   // ambil Data
-  const fetchData = () => {
+  const fetchData = async () => {
     var config = {
       method: "get",
       url: "tasks",
     };
 
-    axios(config)
+    await axios(config)
       .then((response) => {
         const { data } = response;
-        // console.log(data);
         setTask(data);
+        setTasks(data);
       })
       .catch(function (err) {
         console.log(err);
       });
   };
 
-  const removeTodo = () => {
-    alert("suskse");
+  // Regex funtion
+  function escapeRegExp(value) {
+    return value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+  }
+
+  // search
+  const requestSearch = (searchValue) => {
+    const searchRegex = new RegExp(escapeRegExp(searchValue), "i");
+    const filteredRows = tasks.filter((row) => {
+      return Object.keys(row).some((field) => {
+        return searchRegex.test(row[field] ? row[field].toString() : null);
+      });
+    });
+    setTask(filteredRows);
+  };
+
+  // add todo
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    var config = {
+      method: "post",
+      url: "tasks",
+      data: {
+        content,
+      },
+    };
+
+    await axios(config)
+      .then((response) => {
+        alert("sukses menambah todo");
+        setContent("");
+      })
+      .catch(function (err) {
+        console.log(err);
+      })
+      .finally(() => fetchData());
+  };
+
+  // update todo
+  const updateTodo = async (e) => {
+    e.preventDefault();
+    var config = {
+      method: "post",
+      url: `tasks/${idTodo}`,
+      data: {
+        content,
+      },
+    };
+
+    await axios(config)
+      .then((response) => {
+        alert("sukses merubah todo");
+        setContent("");
+        setIsUpdate(false);
+      })
+      .catch(function (err) {
+        console.log(err);
+      })
+      .finally(() => fetchData());
+  };
+
+  // delete todo
+  const removeTodo = async (id) => {
+    var config = {
+      method: "delete",
+      url: `tasks/${id}`,
+    };
+
+    await axios(config)
+      .then((response) => {
+        alert("sukses menghapus todo");
+      })
+      .catch(function (err) {
+        console.log(err);
+      })
+      .finally(() => fetchData());
   };
 
   return (
     <div>
-      <Form />
+      <div className="flex justify-center">
+        <input
+          className="w-full rounded-xl p-2 my-2"
+          type="text"
+          onChange={(e) => requestSearch(e.target.value)}
+          placeholder="search"
+        />
+      </div>
+      <Form
+        onChange={(e) => setContent(e.target.value)}
+        onSubmit={(e) => (isUpdate ? updateTodo(e) : handleSubmit(e))}
+        content={content}
+        isUpdate={isUpdate}
+        cancelUpdate={() => {
+          setContent("");
+          setIsUpdate(false);
+        }}
+      />
       {task.map((data) => (
         <div
           key={data.id}
           className="bg-sky-500 p-2 mt-3 rounded-lg flex justify-between items-center"
         >
-          <h4 className="text-xl">{data.content}</h4>
-          <div className="flex ">
-            <AiFillEdit className="mr-2" size={20} />
+          <h4
+            className="text-xl"
+            onClick={() => navigate(`/detail/${data.id}`)}
+          >
+            {data.content}
+          </h4>
+          <div className="flex">
+            <AiFillEdit
+              className="mr-2"
+              size={20}
+              onClick={() => {
+                setIdTodo(data.id);
+                setIsUpdate(true);
+                setContent(data.content);
+              }}
+            />
             <AiOutlineDelete size={20} onClick={() => removeTodo(data.id)} />
           </div>
         </div>
